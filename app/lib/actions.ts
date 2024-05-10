892a641c508624e123e7eab6192261194c35fcdb
Karol Bracho <brachokarol.sud@gmail.com>
2024-05-10 12:26:24 +0200

    server actions, revalidatepath and redirect - ch12 completed

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,8 +12,9 @@ const FormSchema = z.object({
   status: z.enum(['pending', 'paid']),
   date: z.string(),
 });
-// Omitting id because its going to be generate in the database
+// Use Zod to update the expected types
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
@@ -36,4 +37,28 @@ export async function createInvoice(formData: FormData) {
   revalidatePath('/dashboard/invoices');
   // redirecting to invoices page
   redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
